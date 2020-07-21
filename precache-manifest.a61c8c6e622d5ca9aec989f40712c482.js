a4b1276a8dd1230d56f9adf1d66b3a30a194e8c6
SandroVolonte <61095194+SandroVolonte@users.noreply.github.com>
2020-07-21 20:26:39 +0200

    Updates

---
+++ precache-manifest.a61c8c6e622d5ca9aec989f40712c482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dae66ec9feda391ba7b57f87c99882",
+    "url": "/markdown/index.html"
+  },
+  {
+    "revision": "b2e80db01d040fff37b3",
+    "url": "/markdown/static/css/main.c73e1e1c.chunk.css"
+  },
+  {
+    "revision": "96383a4859b54b5ff1ad",
+    "url": "/markdown/static/js/2.9d70da5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown/static/js/2.9d70da5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e80db01d040fff37b3",
+    "url": "/markdown/static/js/main.c73828e0.chunk.js"
+  },
+  {
+    "revision": "01c0d26099ee35393c10",
+    "url": "/markdown/static/js/runtime-main.a6517e89.js"
+  }
+]);
